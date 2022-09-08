3c89a5901f4edc4d0f4cb4b9de71d2e153f63941
zunayed-arp <zunayed.arp@gmail.com>
2022-09-09 02:11:26 +0600

    chore: post app created

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import CustomUser
+from .forms import CustomUserCreationForm,CustomUserChangeForm
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "name",
+        "is_staff"
+    ]
+    fieldsets=UserAdmin.fieldsets+((None,{"fields":("name",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets+((None,{"fields":("name",)}),)
+    
+admin.site.register(CustomUser,CustomUserAdmin)
