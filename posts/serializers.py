0278b1749f60fd3bf4e125a078d6db698b1bb3bb
zunayed-arp <zunayed.arp@gmail.com>
2022-09-11 10:58:54 +0600

    serializers and urls configured

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,8 +2,7 @@ from rest_framework import serializers
 from .models import Post
 
 
-
 class PostSerializers(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ("id","author","title","body","created_at")
+        fields = ("id", "author", "title", "body", "created_at")
--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import PostList,PostDetail
 
+app_name = "Posts"
 
 urlpatterns= [
-    
+    path("",PostList.as_view(),name="post_list"),
+    path("<int:pk>/",PostDetail.as_view(),name="post_detail"),
 ]
