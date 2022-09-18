b847cf9b4d81296f14daa1d87b13ca2c9ca64796
zunayed-arp <zunayed.arp@gmail.com>
2022-09-18 19:16:36 +0600

    settings file configured

--- blog/settings.py
+++ blog/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-         'rest_framework.permissions.AllowAny',
+        #  'rest_framework.permissions.AllowAny',
+         'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
