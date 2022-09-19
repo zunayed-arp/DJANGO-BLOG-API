c6d826b49645cf5f39ed053645166e3347a68b25
zunayed-arp <zunayed.arp@gmail.com>
2022-09-20 02:36:07 +0600

    refactored views and migrations

--- posts/views.py
+++ posts/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.http import Http404
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
-    context_object_name="posts"
+    context_object_name = "posts"
     paginate_by = 3
-    template_name ='posts/list.html'
-    
-
+    template_name = "posts/list.html"
 
 
 def post_list(request):
@@ -27,13 +27,9 @@ def post_list(request):
     return render(request, "posts/list.html", {"page": page, "posts": posts})
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-        Post,
-        slug=post,
-        status="published",
-        publish__year=year,
-        publish__month=month,
-        publish__day=day,
-    )
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found")
     return render(request, "posts/detail.html", {"post": post})
