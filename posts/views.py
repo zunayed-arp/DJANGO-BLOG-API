8490d070eae099a23674f5b80b9b80f102c8eec3
zunayed-arp <zunayed.arp@gmail.com>
2022-09-19 17:34:28 +0600

    created CBV in views

--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name="posts"
+    paginate_by = 3
+    template_name ='posts/list.html'
+    
+
+
 
 def post_list(request):
     object_list = Post.published.all()
-    paginator = Paginator(object_list,3) #3 posts in each page
-    page = request.GET.get('page')
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get("page")
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-        #if page is not an integer deliver the first page
+        # if page is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
-        #if page is out of range deliver last page of results
+        # if page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, "posts/list.html", {'page':page,"posts": posts})
+    return render(request, "posts/list.html", {"page": page, "posts": posts})
 
 
 def post_detail(request, year, month, day, post):
