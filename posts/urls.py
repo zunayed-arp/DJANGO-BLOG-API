11d8def18042370c876e577d132af2e3115954d8
zunayed-arp <zunayed.arp@gmail.com>
2022-09-20 03:07:51 +0600

    refactored urls,views,html templates

--- posts/urls.py
+++ posts/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import post_list, post_detail,PostListView
+from .views import post_list, post_detail, PostListView
 
-app_name = "Posts"
+app_name = "posts"
 
 urlpatterns = [
     # path("", post_list, name="post_list"),
-    path("",PostListView.as_view(),name="post_list"),
-    path(
-        "<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"
-    ),
+    path("", PostListView.as_view(), name="post_list"),
+    path("<int:id>/", post_detail, name="post_detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -13,18 +13,20 @@ class PostListView(ListView):
 
 
 def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 posts in each page
-    page = request.GET.get("page")
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # if page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request, "posts/list.html", {"page": page, "posts": posts})
+    posts = Post.published.all()
+    return render(request, "posts/list.html", {"posts": posts})
+    # object_list = Post.published.all()
+    # paginator = Paginator(object_list, 3)  # 3 posts in each page
+    # page = request.GET.get("page")
+    # try:
+    #     posts = paginator.page(page)
+    # except PageNotAnInteger:
+    #     # if page is not an integer deliver the first page
+    #     posts = paginator.page(1)
+    # except EmptyPage:
+    #     # if page is out of range deliver last page of results
+    #     posts = paginator.page(paginator.num_pages)
+    # return render(request, "posts/list.html", {"page": page, "posts": posts})
 
 
 def post_detail(request, id):
