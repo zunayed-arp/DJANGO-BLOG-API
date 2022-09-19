a4cfdee19f5d981edf4a2e982a8464ed92cfd5d1
zunayed-arp <zunayed.arp@gmail.com>
2022-09-20 02:35:14 +0600

    refactored admin  and commands text

--- posts/admin.py
+++ posts/admin.py
@@ -5,12 +5,12 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display=('title','slug','author','publish','status',)
-    list_Filter = ('status','created_at','publish','author',)
-    search_fields=('title','body',)
+    list_display=['title','slug','author','publish','status']
+    list_Filter = ['status','created_at','publish','author']
+    search_fields=['title','body']
     prepopulated_fields={'slug':('title',)}
-    raw_id_fields = ('author',)
+    raw_id_fields = ['author']
     date_hierarchy='publish'
-    ordering = ('status','publish',)
+    ordering = ['status','publish']
 
 # admin.site.register(Post)
