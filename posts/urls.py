da8489ed07db55aa80970565a90db07ec2c59599
zunayed-arp <zunayed.arp@gmail.com>
2022-09-19 17:34:12 +0600

    added CBV urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import post_list, post_detail,PostListView
 
 app_name = "Posts"
 
 urlpatterns = [
-    path("", post_list, name="post_list"),
+    # path("", post_list, name="post_list"),
+    path("",PostListView.as_view(),name="post_list"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"
     ),
