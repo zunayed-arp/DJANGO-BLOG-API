fd9abd07fe3f0bc6c09187c8927286c32a2418ad
zunayed-arp <zunayed.arp@gmail.com>
2022-09-19 14:41:13 +0600

    configured static files directory

--- blog/settings.py
+++ blog/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = "blog.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates'],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,7 +127,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR, "static"]
+else:
+    STATIC_ROOT = BASE_DIR, "static"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
