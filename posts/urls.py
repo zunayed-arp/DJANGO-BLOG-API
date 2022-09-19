fc621191a63f521434fcaa942d41765dc5af3d18
zunayed-arp <zunayed.arp@gmail.com>
2022-09-20 04:40:26 +0600

    commented function based view for cbv

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from .views import post_list, post_detail, PostListView
 app_name = "posts"
 
 urlpatterns = [
-    path("", post_list, name="post_list"),
-    # path("", PostListView.as_view(), name="post_list"),
+    # path("", post_list, name="post_list"),
+    path("", PostListView.as_view(), name="post_list"),
     path("<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"),
 ]
