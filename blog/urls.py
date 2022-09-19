196c7da1394a1681290d35c1d47cbbaddd21a042
zunayed-arp <zunayed.arp@gmail.com>
2022-09-19 13:00:37 +0600

    configured urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('posts/',include('posts.urls')),
+    path('posts/',include('posts.urls',namespace='posts')),
     path("api-auth",include("rest_framework.urls")),
 ]
--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import post_list,post_detail
+from .views import post_list, post_detail
+
 app_name = "Posts"
 
-urlpatterns= [
-  path('',post_list,name='post_list'),
-  path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-       post_detail,name="post_detail"),
+urlpatterns = [
+    path("", post_list, name="post_list"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"
+    ),
 ]
