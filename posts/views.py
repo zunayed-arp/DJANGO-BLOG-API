3933686a9f1b0678748adbaac5aff3e14f340a17
zunayed-arp <zunayed.arp@gmail.com>
2022-09-20 04:35:30 +0600

    refactored function based views

--- posts/views.py
+++ posts/views.py
@@ -14,32 +14,33 @@ class PostListView(ListView):
 
 def post_list(request):
     post_list = Post.published.all()
-    #pagination with 3 posts per page
-    # return render(request, "posts/list.html", {"posts": posts})
-    # object_list = Post.published.all()
+    # pagination with 3 posts per page
     paginator = Paginator(post_list, 3)  # 3 posts in each page
-    page_number = request.GET.get('page',1)
-    posts = paginator.page(page_number)
-    # page = request.GET.get("page")
-    # try:
-    #     posts = paginator.page(page)
-    # except PageNotAnInteger:
-    #     # if page is not an integer deliver the first page
-    #     posts = paginator.page(1)
-    # except EmptyPage:
-    #     # if page is out of range deliver last page of results
-    #     posts = paginator.page(paginator.num_pages)
-    return render(request, "posts/list.html", { "posts": posts})
-
-
-def post_detail(request,year,month,day,post):
+    page_number = request.GET.get("page", 1)
     try:
-        post = get_object_or_404(Post,
-                                 status=Post.Status.PUBLISHED,
-                                 slug=post,
-                                 publish__year=year,
-                                 publish__month=month,
-                                 publish__day=day)
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # if page_number is not on integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, "posts/list.html", {"posts": posts})
+
+
+def post_detail(request, year, month, day, post):
+    try:
+        post = get_object_or_404(
+            Post,
+            status=Post.Status.PUBLISHED,
+            slug=post,
+            publish__year=year,
+            publish__month=month,
+            publish__day=day,
+        )
     except Post.DoesNotExist:
         raise Http404("No Post found")
     return render(request, "posts/detail.html", {"post": post})
