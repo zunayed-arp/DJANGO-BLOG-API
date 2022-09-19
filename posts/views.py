1190537dfdd7eb8a34f8332069c220e4a30afc2c
zunayed-arp <zunayed.arp@gmail.com>
2022-09-19 12:38:14 +0600

    added views and html template

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from .serializers import PostSerializers
-
-from rest_framework import generics,permissions
 
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializers
-    
-    
-    
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAdminUser)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializers
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, "posts/list.html", {"posts": posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status="published",
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    return render(request, "posts/detail.html", {"post": post})
