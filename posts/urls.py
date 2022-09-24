90f64ed33749f21fe0ff145ff31a6df5dbfc3c95
zunayed-arp <zunayed.arp@gmail.com>
2022-09-24 14:41:24 +0600

    added new url

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_list, post_detail, PostListView
+from .views import post_list, post_detail, PostListView,post_share
 
 app_name = "posts"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     # path("", post_list, name="post_list"),
     path("", PostListView.as_view(), name="post_list"),
     path("<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"),
+    path('<int:post_id>/share/',post_share,name='post_share'),
 ]
