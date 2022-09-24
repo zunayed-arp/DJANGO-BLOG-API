b549e25158dc28c16b4f993870a8d899b9f08063
zunayed-arp <zunayed.arp@gmail.com>
2022-09-25 01:35:06 +0600

    adding multiple tag,advanced query

--- posts/views.py
+++ posts/views.py
@@ -7,6 +7,7 @@ from django.views.generic import ListView
 from django.http import Http404
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 @require_POST
@@ -72,11 +73,14 @@ def post_share(request, post_id):
 
 
 def post_list(request, tag_slug=None):
+    print("tag_slug", tag_slug)
     post_list = Post.published.all()
     tag = None
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         post_list = post_list.filter(tags__in=[tag])
+
+        print(post_list)
     # pagination with 3 posts per page
     paginator = Paginator(post_list, 3)  # 3 posts in each page
     page_number = request.GET.get("page", 1)
@@ -108,8 +112,24 @@ def post_detail(request, year, month, day, post):
         comments = post.comments.filter(active=True)
         # Form for users to comments
         form = CommentForm()
+
+        # List of similar posts
+        post_tags_ids = post.tags.values_list("id", flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(
+            id=post.id
+        )
+        similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+            "-same_tags", "-publish"
+        )[:4]
     except Post.DoesNotExist:
         raise Http404("No Post found")
     return render(
-        request, "posts/detail.html", {"post": post, "comments": comments, "form": form}
+        request,
+        "posts/detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
